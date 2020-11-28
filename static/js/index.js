a1c4f683d57e9068e0b654119b8fecdc73a73bae
Keunhong Park <k@void.im>
2020-11-27 16:54:58 -0800

    Make pub teasers bigger.

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
     $('#nerfie-image').mouseover(function() {
-        $('#nerfie-image video').css('display', 'inline');
+        $('#nerfie-image video').css('display', 'inline-block');
         $('#nerfie-image img').css('display', 'none');
     });
     $('#nerfie-image').mouseout(function() {
         $('#nerfie-image video').css('display', 'none');
-        $('#nerfie-image img').css('display', 'inline');
+        $('#nerfie-image img').css('display', 'inline-block');
     });
 })
