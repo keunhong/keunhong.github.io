7683e9e4b97c8b2b38a349ffcdff29838f738d96
Keunhong Park <kpar@cs.washington.edu>
2022-05-06 00:03:22 +0900

    Port to Jekyll.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+    $('.publication-mousecell').mouseover(function() {
+        $(this).find('video').css('display', 'inline-block');
+        $(this).find('img').css('display', 'none');
+    });
+    $('.publication-mousecell').mouseout(function() {
+        $(this).find('video').css('display', 'none');
+        $(this).find('img').css('display', 'inline-block');
+    });
+})
