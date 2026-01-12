458ad3044a2e7a2a079b0458cd1579c7a7ec5269
Keunhong Park <k@worldlabs.ai>
2026-01-11 23:13:10 -0800

    new design

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    './*.html',
+  ],
+};
